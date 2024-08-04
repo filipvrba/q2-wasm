c993de0e316c57db63eac0d0f07e1b75bcf01964
Filip Vrba <filipvrba@pm.me>
2024-08-04 16:41:36 +0200

    change port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // nebo jiný port, kde běží Vercel serverless funkce
+        target: 'http://localhost:3001', // nebo jiný port, kde běží Vercel serverless funkce
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
