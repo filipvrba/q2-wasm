edb65409b6203ebe5ad1bbb236d5bec322c8acad
Filip Vrba <filipvrba@pm.me>
2024-08-04 17:05:33 +0200

    lol

--- api/download.js
+++ api/download.js
@@ -5,19 +5,22 @@ export default async function handler(req, res) {
   const fileUrl = `https://drive.google.com/uc?export=download&id=${id}`;
 
   try {
+    // Při stahování velkých souborů může Google Drive vracet stránku s varováním
+    // Využijeme knihovnu Axios pro stahování souborů
     const response = await axios({
       url: fileUrl,
       method: 'GET',
       responseType: 'arraybuffer',
     });
 
-    // Uložení typu obsahu do hlavičky
-    res.setHeader('Content-Type', response.headers['content-type']);
-    res.setHeader('Content-Disposition', `attachment; filename="quake2.data"`); // Nastavení názvu souboru
+    // Kontrola hlaviček a přesměrování, pokud je to nutné
+    res.setHeader('Content-Type', response.headers['content-type'] || 'application/octet-stream');
+    res.setHeader('Content-Disposition', `attachment; filename="quake2.data"`); 
     res.setHeader('Content-Length', response.headers['content-length']);
 
     res.send(response.data);
   } catch (error) {
+    console.error("Error fetching file", error);
     res.status(500).send('Error fetching file');
   }
 }
