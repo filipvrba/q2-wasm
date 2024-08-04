edd76e53f2996a5a88d0da6e8d61124ceefe74d2
Filip Vrba <filipvrba@pm.me>
2024-08-04 18:33:19 +0200

    update vercel fn

--- api/download.js
+++ api/download.js
@@ -1,32 +1,34 @@
-// api/download.js
 import axios from 'axios';
-import { JSDOM } from 'jsdom';  // Používáme jsdom pro parsing HTML
+import cheerio from 'cheerio'; // Používáme cheerio pro parsing HTML
 
 export default async function handler(req, res) {
   const { id } = req.query;
 
   if (!id) {
+    console.error('Missing file ID parameter');
     return res.status(400).json({ error: 'Missing file ID parameter' });
   }
 
   try {
     // 1. Stáhnout HTML obsah
+    console.log(`Fetching HTML content from: https://drive.google.com/uc?export=download&id=${id}`);
     const htmlResponse = await axios.get(`https://drive.google.com/uc?export=download&id=${id}`);
     const htmlContent = htmlResponse.data;
 
     // 2. Parsovat HTML a získat uuid a at
-    const dom = new JSDOM(htmlContent);
-    const document = dom.window.document;
+    const $ = cheerio.load(htmlContent);
+    const uuid = $('input[name=uuid]').val();
+    const at = $('input[name=at]').val();
 
-    const uuid = document.querySelector('input[name="uuid"]').value;
-    const at = document.querySelector('input[name="at"]').value;
+    console.log(`Parsed UUID: ${uuid}`);
+    console.log(`Parsed AT: ${at}`);
 
     // 3. Sestavit URL pro stažení souboru
     const downloadUrl = `https://drive.usercontent.google.com/download?id=${id}&export=download&authuser=0&confirm=t&uuid=${uuid}&at=${at}`;
-
-    res.status(500).send(downloadUrl);
+    console.log(`Download URL: ${downloadUrl}`);
 
     // 4. Stáhnout soubor
+    console.log('Fetching the file from download URL');
     const fileResponse = await axios({
       url: downloadUrl,
       method: 'GET',
@@ -38,6 +40,7 @@ export default async function handler(req, res) {
     res.setHeader('Content-Disposition', `attachment; filename="quake2.data"`);
     res.setHeader('Content-Length', fileResponse.headers['content-length']);
 
+    console.log('Sending file response');
     res.send(fileResponse.data);
   } catch (error) {
     console.error("Error fetching file", error);
