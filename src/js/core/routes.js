3ab8b4c6e82c399c315affb909d5810e9ded243b
Filip Vrba <filipvrba@pm.me>
2024-08-07 01:16:44 +0200

    remove files

--- src/js/core/routes.js
+++
@@ -1,5 +0,0 @@
-import routesObj from "../../json/routes.json";
-window.ROUTES_JSON = routesObj;
-import errorHTML from "../../html/error.html?raw";
-import signinHTML from "../../html/signin.html?raw";
-window.PAGES = {error: errorHTML, signin: signinHTML}
