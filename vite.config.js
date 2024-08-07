2d22e7d92e5d7c1b05f1c0a4087ac59c7391c02c
Filip Vrba <filipvrba@pm.me>
2024-08-07 02:00:25 +0200

    lol

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://0hwjowy5ztxmfwuj8woqzznrotustofnbb316mk1mygk2x8y7u.vercel.app',
+        target: 'https://www.dropbox.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
