ad800fb633b4adadf19da65a5b2734516460ff2a
Filip Vrba <filipvrba@pm.me>
2025-02-11 13:44:19 +0100

    Revert "update favicon"
    This reverts commit 28de7f585c03264e90d488ce1e414fdfcb963449.

--- vite.config.js
+++ vite.config.js
@@ -20,14 +20,14 @@ export default {plugins: [VitePWA({
 
     icons: [
       {
-        src: "./android-chrome-192x192.png",
+        src: "./web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable"
       },
 
       {
-        src: "./android-chrome-512x512.png",
+        src: "./web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable"
